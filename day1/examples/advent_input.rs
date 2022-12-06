f64dd41e27bbe6cbe5bcd0e6b8327809329bab2d
Lucas Franca <lucasfnf@gmail.com>
2022-12-06 22:17:38 +0900

    Rename package of day 1 code

--- day1/examples/advent_input.rs
+++ day1/examples/advent_input.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let res = advent_of_code_2022::find_elf_carrying_most("./day1/resources/input.txt");
+    let res = advent_of_code_2022_day1::find_elf_carrying_most("./day1/resources/input.txt");
     match res {
         Some(res) => println!("{:?}", res),
         None => println!("No result")
